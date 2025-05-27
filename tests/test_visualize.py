a65f8fcf164fb74ee43808e9f7c1c0c190dc59d8
elicasagui <elicasagui@gmail.com>
2025-05-27 10:28:27 -0600

    Update test_visualize.py

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from src.visualize import plot_histogram
 
-def test_plot_histogram(monkeypatch):
+def test_plot_histogram():
     df = pd.DataFrame({'col': [1,2,3]})
-    # Ensure no errors when plotting
+  
     plot_histogram(df, 'col', bins=3)
